19e036e33ac91e3195de8cc41ec3663a6edef677
zaidkh03 <alkhalili208@gmail.com>
2025-05-21 16:28:02 +0300

    first finle submission

--- README.md
+++
@@ -1,3 +0,0 @@
-# finalVersion
-
-School Management System
--- website/webpages/teacher/components/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2025-Presant <a href="https://adminlte.io">Madrasati</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 1.0.0
-    </div>
-</footer>
